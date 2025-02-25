86a87a6246600cc291d6a3db184abd5996fcc2b9
mjordan <michael@glowingpixel.com>
2025-02-24 17:39:04 -0800

    Read multiple

--- examples/read_ale.py
+++ examples/read_ale.py
@@ -4,12 +4,18 @@ import pyale
 if __name__ == "__main__":
 
 	if not len(sys.argv) > 1:
-		print(f"Usage: {pathlib.Path(__file__).name} file_path.ale", file=sys.stderr)
+		print(f"Usage: {pathlib.Path(__file__).name} file_path.ale [another_file.ale ...]", file=sys.stderr)
 		sys.exit(1)
 
-	ale = pyale.Ale.from_path(sys.argv[1])
-	event_columns = ale.events.columns
-	print(f"ALE Header Columns ({len(ale.columns)}): {[col for col in ale.columns]}")
-	print(f" ALE Event Columns ({len(event_columns)}): {event_columns}")
-	print(f"        ALE Events ({len(ale.events)}): {[e for e in ale.events]}")
-	print(f"Using trailing tab? {ale.columns.with_trailing_tab}")
+	for path_ale in sys.argv[1:]:
+		try:
+			ale = pyale.Ale.from_path(path_ale)
+		except Exception as e:
+			print(f"Skipping {path_ale}: {e}")
+			continue
+		
+		print("")
+		print(f"Ale Path:  {path_ale}")
+		print(f"ALE Header Columns ({len(ale.columns)}):  {[col for col in ale.columns]}")
+		print(f" ALE Event Columns ({len(ale.events.columns)}):  {ale.events.columns}")
+		print(f"        ALE Events ({len(ale.events)}):  {[e for e in ale.events]}")
